152a11b4ea8919731387feb54cc8999337110202
Kiran9527 <43056819+Kiran952@users.noreply.github.com>
2021-05-13 15:14:26 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyqrcode # here pyqrcode is the module for to generate qrcode in python
+from pyqrcode import QRCode
+s = "https://www.youtube.com/watch?v=R4AbzwYOmNE"   #url link
+url = pyqrcode.create(s)     #creating qrcode from url link
+url.svg("myyoutube.svg",scale=8)
+
+
+#scale = dimensions for code
+#SVG stands for scalable vector graphics, and it is a file format
+# that allows you to display vector images on your website
+
